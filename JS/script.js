1fa24b881390d96804815e3bd9f3ac3b4290c78c
rom <roman.ramos.322@gmail.com>
2026-04-28 16:04:36 -0600

    primera version de login

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+function login() {
+    let divMensaje = document.querySelector('#mensaje');
+    let usuario = document.querySelectorAll('.entradaTexto')[0].value;
+    let contra = document.querySelectorAll('.entradaTexto')[1].value;
+
+    if (usuario === "equipo2" && contra === "123") {
+        window.location.href = "pages/principal.html";
+    } else {
+        divMensaje.innerHTML = "Acceso Denegado. Usuario o contraseña incorrectos.";
+        divMensaje.style.backgroundColor = "#f8d7da";
+        divMensaje.style.border = "2px solid #f5c6cb";
+        divMensaje.style.padding = "10px";
+        divMensaje.style.borderRadius = "10px";
+        divMensaje.style.color = "#721c24";;
+    }
+}
+
+const btnIngresar = document.getElementById('btnIngresar');
+btnIngresar.addEventListener("click", login);
